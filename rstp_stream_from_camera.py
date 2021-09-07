6a6c0fd687a3f21e8ede74a89d03c6432b9da7c6
Big <sorravit.bunjongpean@refinitiv.com>
2021-09-08 03:19:31 +0700

    FPS
    (cherry picked from commit a1a0834bb94f21c4c22d620d20a273d240adbf6c)

--- rstp_stream_from_camera.py
+++ rstp_stream_from_camera.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 import os
 
@@ -8,12 +10,15 @@ rtsp_username = os.environ.get('rtspUsername')
 rtsp_password = os.environ.get('rtspPassword')
 rtsp_address = os.environ.get('rtspAddress')
 
-# cap = cv2.VideoCapture("rtsp://{}:{}@{}".format(rtsp_username, rtsp_password, rtsp_address))
-cap = cv2.VideoCapture(0)
-
+cap = cv2.VideoCapture("rtsp://{}:{}@{}".format(rtsp_username, rtsp_password, rtsp_address))
+# cap = cv2.VideoCapture(0)
+pTime = 0
 while cap.isOpened():
     ret, frame = cap.read()
-    cv2.putText(frame, "HEllooo", (20, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 0), 2)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(frame, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
