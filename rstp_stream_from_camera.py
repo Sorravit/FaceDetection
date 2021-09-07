6a9589af3146b70954368c6a0dc383522b4b913c
Big <sorravit.bunjongpean@refinitiv.com>
2021-09-08 03:16:28 +0700

    Put some text there because I can
    (cherry picked from commit af0e6ea8b2134dd23dc37dce599558300e74e42a)

--- rstp_stream_from_camera.py
+++ rstp_stream_from_camera.py
@@ -7,10 +7,13 @@ load_dotenv()
 rtsp_username = os.environ.get('rtspUsername')
 rtsp_password = os.environ.get('rtspPassword')
 rtsp_address = os.environ.get('rtspAddress')
-cap = cv2.VideoCapture("rtsp://{}:{}@{}".format(rtsp_username, rtsp_password, rtsp_address))
-# cap = cv2.VideoCapture(0)
-while (cap.isOpened()):
+
+# cap = cv2.VideoCapture("rtsp://{}:{}@{}".format(rtsp_username, rtsp_password, rtsp_address))
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
     ret, frame = cap.read()
+    cv2.putText(frame, "HEllooo", (20, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 0), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
