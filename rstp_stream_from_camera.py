80440bc2f0292d88c1c452ba4106638fb9f8a1b4
Big <sorravit.bunjongpean@refinitiv.com>
2021-09-08 04:26:28 +0700

    Show confident interval in the VDO
    (cherry picked from commit 6d8206762650cb9f4744895b3271f369e3c85867)

--- rstp_stream_from_camera.py
+++ rstp_stream_from_camera.py
@@ -32,13 +32,15 @@ while cap.isOpened():
             ih, iw, ic = img.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih),
             cv2.rectangle(img, bbox, (0, 255, 123), 2)
+            cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_COMPLEX, 3,
+                        (0, 255, 255), 2)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
     cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255), 2)
-    cv2.imshow('frame', img)
+    cv2.imshow('My VDO', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
